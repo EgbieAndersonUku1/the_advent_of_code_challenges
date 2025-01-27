87b958a0d36736c24d64f35c44948af65ef732de
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2025-01-27 10:53:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🎄 Advent of Code 🎄
+
+Welcome to my Advent of Code solutions repository! 🎅✨
+
+Advent of Code is an annual coding challenge that runs from December 1st to December 25th, with a new puzzle released every day. The problems are a great way to practice problem-solving skills, algorithms, and data structures.
+
+## 📜 About
+
+This repository contains my solutions to the puzzles for Advent of Code but starting from the year 2022. Each puzzle is available at [adventofcode.com](https://adventofcode.com/2022),
+
+
+#### To run the code git clone then navigate to the folder, find the py file and run `python <filename_here.py>` replacing the `filename_here.py` with file name found in the directory
---
+++ calories/elves_calories.py
@@ -0,0 +1,97 @@
+class Cache:
+    def __init__(self):
+        self.cache = []
+    
+    def get_cache(self):
+        return self.cache
+    
+    def add_cache(self, value):
+        if isinstance(value, list):
+            self.cache.extend(value)
+        else:
+            self.cache.append(value)
+
+
+def get_problem_file(text_file="problem_input.txt", mode="r"):
+    """Reads the problem input file and returns its lines."""
+    try:
+        with open(text_file, mode=mode) as f:
+            return f.readlines()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file '{text_file}' was not found.")
+    except Exception as e:
+        raise RuntimeError(f"An error occurred while reading the file: {e}")
+    
+    
+def get_elves_calories_count() -> dict:
+    """Parses the input file and calculates total calories per elf."""
+    
+    calories_list     = get_problem_file()
+    calories_count    = 0
+    num_of_elves_seen = 0
+    
+    elves = {}
+    
+    for calories in calories_list:
+        calories = calories.strip("\n")
+       
+        if calories.isdigit():
+            calories_count += int(calories)
+        else:
+            num_of_elves_seen += 1
+            elves[num_of_elves_seen] = calories_count
+                           
+            calories_count = 0
+            
+    return elves
+    
+
+def sort_calories_by_highest(elves:dict) -> list:
+    """Sorts elves' calorie counts in descending order."""
+
+    if not isinstance(elves, dict):
+        raise ValueError(f"Expected a dictionary but got type: {type(elves)}")
+    
+    calories = cache.get_cache()
+    if calories:
+        return calories
+    
+    calories = sorted(elves.values(), reverse=True)
+    cache.add_cache(calories)
+    return calories
+
+
+def get_the_highest_medium_and_lowest_calories_total(calories_list: list) -> int:
+    """
+    Returns the sum of the top three calorie values.
+    
+    Args:
+        calories_list: A list of integers contains calories
+    
+    :Returns:
+        The total sum of the top three calorie values
+    """
+    if not isinstance(calories_list, list):
+        raise ValueError("The list cannot be empty nor cannot be a list")
+    if len(calories_list) < 3:
+        raise ValueError("The list must have at least the three integers")
+    
+    return sum(calories_list[:3])
+
+
+def main():
+    """The main function that runs the file"""
+    
+    elves         = get_elves_calories_count()
+    calories_list = sort_calories_by_highest(elves)
+
+    highest_calories = calories_list[0]
+    total            = get_the_highest_medium_and_lowest_calories_total(calories_list)
+
+    print(f"The highest calorie count is: {highest_calories} ")
+    print(f"The top three highest calorie count is:  {total}")
+
+if __name__ == "__main__":
+    
+    cache = Cache()
+    main()
